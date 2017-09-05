bcf2bfa5eabe067727107c1ea9ec7d152488e31c
beanstown106 <nbenis106@gmail.com>
2017-09-05 14:44:44 +0800

    ParasiteSettings: Incall vibration option [3/3]
    * allow setting vibration when call is connected
    * allow setting vibration when call is disconnected
    * allow setting vibration for call waiting
    * this works with google and aosp dialer :)
    
    Adapted to PE
    
    [arrow-13.0]:
    - Move inside accessibility_vibration_settings
      as all the aosp vibration toggles are there
    
    includes:
    fixup! Settings: incall vibration options [3/3]
    
    [ghostrider-reborn]
    - Enable by default
    - Add to the slider-based prefs page as well
    
    Change-Id: I55e99f501a3af07287c57a23939666351f4cef67
    Signed-off-by: Karan Parashar <whyredfire@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- ParasiteSettings/src/com/android/settings/custom/sound/SoundSettings.java
+++ ParasiteSettings/src/com/android/settings/custom/sound/SoundSettings.java
@@ -2,13 +2,31 @@ package com.android.settings.custom.sound;
 
 import android.os.Bundle;
 
+import android.content.Context;
+
 import com.android.settings.custom.R;
 import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceCategory;
+
+import android.telephony.TelephonyManager;
 
 public class SoundSettings extends PreferenceFragment {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.sound_settings);
+
+        final PreferenceCategory incallVibrationCategory =
+                (PreferenceCategory) getPreferenceScreen().findPreference("accessibility_incall_vibration_category");
+
+        if (!isVoiceCapable()) {
+            getPreferenceScreen().removePreference(incallVibrationCategory);
+        }
+    }
+
+    private boolean isVoiceCapable() {
+        TelephonyManager telephony =
+                (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
+        return telephony != null && telephony.isVoiceCapable();
     }
 }

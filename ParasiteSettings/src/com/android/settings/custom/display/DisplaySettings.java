8791aacc5f2454f421ed882ab556d2240281e369
LuK1337 <priv.luk@gmail.com>
2018-03-11 11:38:52 +0100

    ParasiteSettings: Add high touch sensitivity and touchscreen hovering toggles
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Tue Mar 26 00:06:11 2019 +0000
    
        DisplaySettings: Hide high touch sensitivity from search if needed
    
         * Fixes: https://gitlab.com/LineageOS/issues/android/issues/260
    
        Change-Id: I98ce66dec9801c36da5a2facd6b033895b7963f0
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Tue Mar 26 00:20:37 2019 +0000
    
        LanguageAndInputSettings: Hide touch hovering from search if needed
    
        Change-Id: I5d53946262919ff67be5e777654af2171ffd2a44
    
    Change-Id: Ib8712d2d0c0fe4396dbea7c7fc130e0c253d3207

--- ParasiteSettings/src/com/android/settings/custom/display/DisplaySettings.java
+++ ParasiteSettings/src/com/android/settings/custom/display/DisplaySettings.java
@@ -6,15 +6,34 @@ import com.android.settings.custom.R;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragment;
 
+import com.android.internal.custom.hardware.LineageHardwareManager;
 import com.android.internal.util.custom.cutout.CutoutFullscreenController;
 
 public class DisplaySettings extends PreferenceFragment {
     private static final String CUTOUT_FROCE_FULL_SCREEN_PREF = "display_cutout_force_fullscreen_settings";
     private CutoutFullscreenController mCutoutForceFullscreenSettings;
 
+    private static final String KEY_HIGH_TOUCH_SENSITIVITY = "high_touch_sensitivity_enable";
+    private static final String KEY_TOUCH_HOVERING = "feature_touch_hovering";
+    private LineageHardwareManager mHardware;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.display_settings);
+
+        final Preference highTouchSensitivityPref =
+                (Preference) getPreferenceScreen().findPreference(KEY_HIGH_TOUCH_SENSITIVITY);
+        final Preference touchHoveringPref =
+                (Preference) getPreferenceScreen().findPreference(KEY_TOUCH_HOVERING);
+
+        mHardware = LineageHardwareManager.getInstance(getActivity());
+
+        if (!mHardware.isSupported(LineageHardwareManager.FEATURE_HIGH_TOUCH_SENSITIVITY)){
+            getPreferenceScreen().removePreference(highTouchSensitivityPref);
+        }
+        if (!mHardware.isSupported(LineageHardwareManager.FEATURE_TOUCH_HOVERING)){
+            getPreferenceScreen().removePreference(touchHoveringPref);
+        }
 
         mCutoutForceFullscreenSettings = new CutoutFullscreenController(getContext());
 

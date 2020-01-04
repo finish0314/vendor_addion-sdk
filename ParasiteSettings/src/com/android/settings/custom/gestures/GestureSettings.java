689e55dc5689ab6661acbd882c9179dcaebffcee
Anushek Prasal <anushekprasal@gmail.com>
2020-01-04 20:00:01 +0530

    ParasiteSettings: Add preference for TouchGestures
    Change-Id: Iaab7dece83c6c4804bf2cc2706b39143804f003b

--- ParasiteSettings/src/com/android/settings/custom/gestures/GestureSettings.java
+++ ParasiteSettings/src/com/android/settings/custom/gestures/GestureSettings.java
@@ -3,12 +3,25 @@ package com.android.settings.custom.gesture;
 import android.os.Bundle;
 
 import com.android.settings.custom.R;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragment;
 
+import com.android.internal.custom.hardware.LineageHardwareManager;
+
 public class GestureSettings extends PreferenceFragment {
+    private static final String TOUCHSCREEN_GESTURES_PREF = "touchscreen_gesture_settings";
+    private LineageHardwareManager mHardware;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.gesture_settings);
+
+        final Preference touchscreenGesturesPref =
+                (Preference) getPreferenceScreen().findPreference(TOUCHSCREEN_GESTURES_PREF);
+
+        mHardware = LineageHardwareManager.getInstance(getActivity());
+        if (!mHardware.isSupported(LineageHardwareManager.FEATURE_TOUCHSCREEN_GESTURES)){
+            getPreferenceScreen().removePreference(touchscreenGesturesPref);
+        }
     }
 }

0f74e45485dd722d80b33ebc1d6212235135efb7
jhenrique09 <jhenrique09@users.noreply.github.com>
2020-10-03 20:05:46 +0000

    ParasiteSettings: Implement cutout force full screen [2/2]
    Inspired by MIUI and Essential
    
    Squashed commits:
    
    commit e483acceb29cdd70bd649a732a03780dacf73a6c
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Sat Sep 29 01:02:32 2018 +0200
    
        LineageParts: Remove dividers from expanded desktop/long screen apps list
    
        Change-Id: I699f7c20d8313ad779165db336aed7bc72e68d28
    
    commit f9304f0cdc0e92dac787e6d414d36de51cdbed85
    Author: Tony Mantler <nicoya@google.com>
    Date:   Thu Sep 21 15:50:27 2017 -0700
    
        LineageParts: Make ApplicationsState.Session lifecycle-aware
    
        Bug: 66682989
        Test: RunSettingsRoboTests
        Change-Id: Idf56788fcae22639b7bf604a8c73cd5f0ff2da16
    
    commit 1b921f454a4d4c30317b72958251e83ff3792d81
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Mon Jun 4 16:01:00 2018 +0200
    
        LineageParts: Add an option to force pre-O apps to use full screen aspect ratio
    
        When an app target pre-O releases, the default max aspect ratio
        is 1.86:1 which leads to ugly black areas on devices that have
        screens with higher aspect ratio (for example Galaxy S8/S9).
    
        This change adds an option to allow users to change aspect ratio
        for pre-O apps to full screen aspect ratio.
    
        Change-Id: I20eeabf140c9d278d21d68675aed856d45b2f78a
    
    Change-Id: I5f7a769e13ee74071f886f599797284238eb6ab3

--- ParasiteSettings/src/com/android/settings/custom/display/DisplaySettings.java
+++ ParasiteSettings/src/com/android/settings/custom/display/DisplaySettings.java
@@ -3,12 +3,26 @@ package com.android.settings.custom.display;
 import android.os.Bundle;
 
 import com.android.settings.custom.R;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragment;
 
+import com.android.internal.util.custom.cutout.CutoutFullscreenController;
+
 public class DisplaySettings extends PreferenceFragment {
+    private static final String CUTOUT_FROCE_FULL_SCREEN_PREF = "display_cutout_force_fullscreen_settings";
+    private CutoutFullscreenController mCutoutForceFullscreenSettings;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.display_settings);
+
+        mCutoutForceFullscreenSettings = new CutoutFullscreenController(getContext());
+
+        final Preference displayCutoutForceFullScreenPref =
+                (Preference) getPreferenceScreen().findPreference(CUTOUT_FROCE_FULL_SCREEN_PREF);
+
+        if (!mCutoutForceFullscreenSettings.isSupported()) {
+            getPreferenceScreen().removePreference(displayCutoutForceFullScreenPref);
+        }
     }
 }

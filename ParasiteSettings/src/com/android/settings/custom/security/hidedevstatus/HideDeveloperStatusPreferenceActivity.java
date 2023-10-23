484a925d5c5ed00cedc5f8424743151eb4fbacc7
someone5678 <someone5678@users.noreply.github.com>
2023-10-23 16:22:07 +0900

    ParasiteSettings: HideDeveloperStatus: Query all apps installed by any user [2/2]
    * And set value for all user
    
    Change-Id: I8a8c830e21f45941ab05a39e127a20a99919393f

--- ParasiteSettings/src/com/android/settings/custom/security/hidedevstatus/HideDeveloperStatusPreferenceActivity.java
+++ ParasiteSettings/src/com/android/settings/custom/security/hidedevstatus/HideDeveloperStatusPreferenceActivity.java
@@ -17,25 +17,35 @@
 package com.android.settings.custom.security.hidedevstatus;
 
 import android.content.Context;
+import android.content.pm.UserInfo;
 import android.provider.Settings;
 import android.os.Bundle;
-import android.os.UserHandle;
+import android.os.UserManager;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 import com.android.settingslib.widget.R;
 
 import com.android.internal.util.custom.HideDeveloperStatusUtils;
 
+import java.util.List;
+
 public class HideDeveloperStatusPreferenceActivity extends CollapsingToolbarBaseActivity {
 
     private static final String TAG = "hide_developer_status_settings";
     private static HideDeveloperStatusUtils hideDeveloperStatusUtils = new HideDeveloperStatusUtils();
 
+    private UserManager userManager;
+    private List<UserInfo> userInfos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        hideDeveloperStatusUtils.setApps(getContext());
+        userManager = UserManager.get(getApplicationContext());
+        userInfos = userManager.getUsers();
+        for (UserInfo info: userInfos) {
+            hideDeveloperStatusUtils.setApps(getApplicationContext(), info.id);
+        }
 
         getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,
                 new HideDeveloperStatusSettings(), TAG)

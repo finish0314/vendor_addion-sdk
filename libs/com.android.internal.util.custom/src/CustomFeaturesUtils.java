d4c9dfd2577226f65200aa1f0e79f8796ff0244d
someone5678 <someone5678@users.noreply.github.com>
2024-03-07 20:29:17 +0900

    parasite: Add CustomFeaturesUtils
    Change-Id: I0814900d7c9a966a2dd13cce3f11a5355e566c05

---
+++ libs/com.android.internal.util.custom/src/CustomFeaturesUtils.java
@@ -0,0 +1,10 @@
+package com.android.internal.util.custom;
+
+import android.app.ActivityThread;
+
+import java.util.Arrays;
+
+public class CustomFeaturesUtils {
+    public boolean hasSystemFeatureCustom(String name) {
+    }
+}

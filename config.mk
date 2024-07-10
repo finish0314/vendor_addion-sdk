670bd3562179d59ffc8df54e61e58b9caa9417b2
someone5678 <someone5678@users.noreply.github.com>
2024-07-11 00:32:23 +0900

    Comment out some stuffs
    Change-Id: I9ef7d54a7645a06843a74d712e297e0f4d6eeb44

--- config.mk
+++ config.mk
@@ -35,7 +35,7 @@ $(call inherit-product-if-exists, vendor/parasite/signatures/config.mk)
 $(call inherit-product-if-exists, vendor/parasite/prebuilts/config.mk)
 
 # GMS
-$(call inherit-product, vendor/google/gms/products/gms.mk)
+#$(call inherit-product, vendor/google/gms/products/gms.mk)
 
 # Microsoft
-$(call inherit-product-if-exists, vendor/microsoft/mms/products/mms.mk)
+#$(call inherit-product-if-exists, vendor/microsoft/mms/products/mms.mk)

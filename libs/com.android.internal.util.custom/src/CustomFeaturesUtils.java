788569ea97f4433ebb2aa2f415e97fe8ad575bce
someone5678 <someone5678@users.noreply.github.com>
2024-08-20 15:08:50 +0900

    CustomFeaturesUtils: Reduce indent
    Change-Id: Ic6041959e71de32649b7414bc1309c0c4de63e7e

--- libs/com.android.internal.util.custom/src/CustomFeaturesUtils.java
+++ libs/com.android.internal.util.custom/src/CustomFeaturesUtils.java
@@ -20,47 +20,47 @@ public final class CustomFeaturesUtils {
             SystemProperties.get("ro.soc.manufacturer", "").toLowerCase().contains("google");
 
     private static final String[] featuresPixel = {
-            "com.google.android.apps.photos.PIXEL_2017_PRELOAD",
-            "com.google.android.apps.photos.PIXEL_2018_PRELOAD",
-            "com.google.android.apps.photos.PIXEL_2019_MIDYEAR_PRELOAD",
-            "com.google.android.apps.photos.PIXEL_2019_PRELOAD",
-            "com.google.android.feature.PIXEL_2017_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2018_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2019_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2019_MIDYEAR_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2020_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2020_MIDYEAR_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2021_MIDYEAR_EXPERIENCE",
+        "com.google.android.apps.photos.PIXEL_2017_PRELOAD",
+        "com.google.android.apps.photos.PIXEL_2018_PRELOAD",
+        "com.google.android.apps.photos.PIXEL_2019_MIDYEAR_PRELOAD",
+        "com.google.android.apps.photos.PIXEL_2019_PRELOAD",
+        "com.google.android.feature.PIXEL_2017_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2018_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2019_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2019_MIDYEAR_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2020_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2020_MIDYEAR_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2021_MIDYEAR_EXPERIENCE",
     };
 
     private static final String[] featuresPixelOthers = {
-            "com.google.android.feature.ANDROID_ONE_EXPERIENCE",
-            "com.google.android.feature.ASI",
-            "com.google.lens.feature.CAMERA_INTEGRATION",
-            "com.google.lens.feature.IMAGE_INTEGRATION",    
-            "com.google.photos.trust_debug_certs",
+        "com.google.android.feature.ANDROID_ONE_EXPERIENCE",
+        "com.google.android.feature.ASI",
+        "com.google.lens.feature.CAMERA_INTEGRATION",
+        "com.google.lens.feature.IMAGE_INTEGRATION",    
+        "com.google.photos.trust_debug_certs",
     };
 
     private static final String[] featuresTensor = {
-            "com.google.android.feature.PIXEL_2021_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2022_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2022_MIDYEAR_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2023_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2023_MIDYEAR_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2024_EXPERIENCE",
-            "com.google.android.feature.PIXEL_2024_MIDYEAR_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2021_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2022_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2022_MIDYEAR_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2023_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2023_MIDYEAR_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2024_EXPERIENCE",
+        "com.google.android.feature.PIXEL_2024_MIDYEAR_EXPERIENCE",
     };
 
     private static final String[] featuresNexus = {
-            "com.google.android.apps.photos.NEXUS_PRELOAD",
-            "com.google.android.apps.photos.nexus_preload",
-            "com.google.android.feature.GOOGLE_BUILD",
-            "com.google.android.feature.GOOGLE_EXPERIENCE",
-            "com.google.android.feature.PIXEL_EXPERIENCE",
+        "com.google.android.apps.photos.NEXUS_PRELOAD",
+        "com.google.android.apps.photos.nexus_preload",
+        "com.google.android.feature.GOOGLE_BUILD",
+        "com.google.android.feature.GOOGLE_EXPERIENCE",
+        "com.google.android.feature.PIXEL_EXPERIENCE",
     };
 
     private static final String[] featuresAndroid = {
-            "android.software.freeform_window_management"
+        "android.software.freeform_window_management"
     };
 
     public static int hasSystemFeatureCustom(String name) {
